be39c7e7ed09ff88f6d8213019d4d5e37ea3198f
ann <ann.addicks@gmail.com>
2015-04-17 08:06:18 -0400

    use java 8 LocalDateTime and  add a data field for powershell.

--- src/main/java/com/khoubyari/example/domain/Status.java
+++ src/main/java/com/khoubyari/example/domain/Status.java
@@ -1,5 +1,7 @@
 package com.khoubyari.example.domain;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by ann on 4/16/15.
  */
@@ -7,13 +9,27 @@ public class Status {
 
     private final Boolean isUpdateNeeded;
 
+    private final LocalDateTime timestamp;
+
+    private static final String data =  "get-process";
+
 
 
     public Status(Boolean isUpdateNeeded) {
         this.isUpdateNeeded = isUpdateNeeded;
+        timestamp = LocalDateTime.now();
+
     }
 
     public Boolean isUpdateNeeded() {
         return isUpdateNeeded;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public static String getData() {
+        return data;
     }
 }
