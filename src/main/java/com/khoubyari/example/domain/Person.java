6bce20efcd6e4bbc53a3ed35eeab8259ddc3b5ba
ann <ann.addicks@gmail.com>
2015-04-28 13:17:41 -0400

    converted id to UUID and added constraints to the person table.

--- src/main/java/com/khoubyari/example/domain/Person.java
+++ src/main/java/com/khoubyari/example/domain/Person.java
@@ -13,8 +13,27 @@ public class Person {
     @Id
     @GeneratedValue
     private UUID id;
+    private String email;
     private String firstName;
     private String lastName;
+
+    public Person() {}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getFirstName() {
         return this.firstName;
--- src/main/resources/db/migration/V1__Create_AppUser_Table.sql
+++ src/main/resources/db/migration/V1__Create_AppUser_Table.sql
@@ -1,7 +1,10 @@
 CREATE TABLE PERSON (
 	id CHAR(36) NOT Null,
-	email varchar(255) not null UNIQUE,
-	first_name varchar(255) not null,
-	last_name varchar(255) not null
+	email varchar(255),
+	first_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL,
+	PRIMARY KEY (email)
 );
 
+CREATE INDEX personId
+    ON Person(id);
