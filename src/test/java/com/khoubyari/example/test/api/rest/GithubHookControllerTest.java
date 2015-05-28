bddba891041ed5f200ba7ed49e021b944dd7d83a
ann <ann.addicks@gmail.com>
2015-05-27 20:43:36 -0400

    test the github controller

--- src/test/java/com/khoubyari/example/test/api/rest/GithubHookControllerTest.java
+++ src/test/java/com/khoubyari/example/test/api/rest/GithubHookControllerTest.java
@@ -1,14 +1,18 @@
 package com.khoubyari.example.test.api.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khoubyari.example.Application;
 import com.khoubyari.example.api.rest.GithubHookController;
 import com.khoubyari.example.api.rest.HotelController;
 import com.khoubyari.example.domain.Hotel;
+import com.khoubyari.example.domain.Payload;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.junit.FlywayTestExecutionListener;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
@@ -20,10 +24,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.regex.Pattern;
+
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertTrue;
@@ -52,29 +61,34 @@ public class GithubHookControllerTest {
 
     private static final String RESOURCE_LOCATION_PATTERN = "http://localhost/example/v1/hotels/[0-9]+";
 
+    @Before
+    public void initTests() {
+        MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+
     @Test
     public void shouldCreate() throws Exception {
 
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("json/githubPayload.json").getFile());
+        InputStream is = new FileInputStream(file);
+        byte[] json = toJson(file);
+
 
         //CREATE
-        MvcResult result = mvc.perform(post("/example/v1/hotels")
-                .content(r1Json)
+       /* MvcResult result = mvc.perform(post(GithubHookController.REQUEST_MAPPING)
+                .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
+                .andExpect(status().isOk())
                 .andExpect(redirectedUrlPattern(RESOURCE_LOCATION_PATTERN))
                 .andReturn();
-        long id = getResourceIdFromUrl(result.getResponse().getRedirectedUrl());
 
-
+*/
     }
 
-    private long getResourceIdFromUrl(String locationUrl) {
-        String[] parts = locationUrl.split("/");
-        return Long.valueOf(parts[parts.length - 1]);
-    }
 
     // match redirect header URL (aka Location header)
     private static ResultMatcher redirectedUrlPattern(final String expectedUrlPattern) {
@@ -84,6 +98,11 @@ public class GithubHookControllerTest {
                 assertTrue(pattern.matcher(result.getResponse().getRedirectedUrl()).find());
             }
         };
+    }
+
+    private byte[] toJson(Object r) throws Exception {
+        ObjectMapper map = new ObjectMapper();
+        return map.writeValueAsString(r).getBytes();
     }
 
 
