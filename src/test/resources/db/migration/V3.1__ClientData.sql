88664fefb283878e9b6db22031419f493173844a
ann <ann.addicks@gmail.com>
2015-07-20 14:27:10 -0400

    adding test data.

---
+++ src/test/resources/db/migration/V3.1__ClientData.sql
@@ -0,0 +1,2 @@
+INSERT INTO Client (id, name) VALUES ('54fe6e4c-f45a-43ef-8ca7-d2219b5090b4', 'Client 1');
+INSERT INTO Client (id, name) VALUES ('54fe6e4c-f45a-43ef-8ca7-d2219b5090b9', '2 Client');
