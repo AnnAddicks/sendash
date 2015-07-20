0ae109a444319d1252d8c753385584f489918985
ann <ann.addicks@gmail.com>
2015-07-20 14:38:42 -0400

    more test data.

---
+++ src/test/resources/db/migration/V6.1__ScriptData.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO Script (id, scriptName, scriptLastUpdated, data) VALUES(1, 'First Script', Now(), 'get-process');
+INSERT INTO Script (id, scriptName, scriptLastUpdated, data) VALUES(2, 'Second Script', Now(), 'get-process');
---
+++ src/test/resources/db/migration/V9.1__EndpointScriptData.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO EndpointScript (endpointId, scriptId) VALUES ('zzfe6e4c-f45a-43ef-8ca7-d2219b509099', 1);
+INSERT INTO EndpointScript (endpointId, scriptId) VALUES ('zzfe6e4c-f45a-43ef-8ca7-d2219b509099', 2);
