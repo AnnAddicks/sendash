ca16b719763e4b28362b640d74362294a4a0cfa2
AnnAddicks <Ann.Addicks@gmail.com>
2015-08-20 10:30:28 -0400

    commenting out the properties reference.  Keeping this file around as an example until I imlement one for sendash.

--- src/main/java/com/khoubyari/example/service/HotelServiceHealth.java
+++ src/main/java/com/khoubyari/example/service/HotelServiceHealth.java
@@ -13,13 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class HotelServiceHealth implements HealthIndicator {
 
-    @Autowired
-    private ServiceProperties configuration;
+    //@Autowired
+    //private ServiceProperties configuration;
 
     // extend this to create an application-specific health check according to http://goo.gl/vt8I7O
     @Override
     public Health health() {
-        return Health.up().withDetail("details", "{ 'internals' : 'getting close to limit', 'profile' : '" + this.configuration.getName() + "' }").status("itsok!").build();
+    	return Health.up().withDetail("","").status("").build();
+        //return Health.up().withDetail("details", "{ 'internals' : 'getting close to limit', 'profile' : '" + this.configuration.getName() + "' }").status("itsok!").build();
     }
 
 }
