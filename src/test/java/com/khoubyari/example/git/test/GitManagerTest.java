a56e22e6d336dadb53edd8a329a40d97b4b20677
AnnAddicks <Ann.Addicks@gmail.com>
2015-08-20 14:19:29 -0400

    adding a test for the repo manager.

---
+++ src/test/java/com/khoubyari/example/git/test/GitManagerTest.java
@@ -0,0 +1,14 @@
+package com.khoubyari.example.git.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GitManagerTest {
+
+	@Test
+	public void testUpdateLocalRepository() {
+		fail("Not yet implemented");
+	}
+
+}
