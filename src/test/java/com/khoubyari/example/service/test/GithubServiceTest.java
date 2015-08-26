9f08fc3f940438b6d6814428f1878dc6d8966772
AnnAddicks <Ann.Addicks@gmail.com>
2015-08-26 15:07:51 -0400

    start testing the service.

--- src/test/java/com/khoubyari/example/service/test/GithubServiceTest.java
+++ src/test/java/com/khoubyari/example/service/test/GithubServiceTest.java
@@ -1,5 +1,9 @@
 package com.khoubyari.example.service.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Calendar;
+
 import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.junit.FlywayTestExecutionListener;
 import org.junit.Test;
@@ -14,31 +18,43 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.khoubyari.example.Application;
+import com.khoubyari.example.domain.Payload;
 import com.khoubyari.example.service.GithubService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = Application.class)
 @Profile("test")
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-    TransactionalTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
-    DbUnitTestExecutionListener.class, FlywayTestExecutionListener.class })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class, DbUnitTestExecutionListener.class,
+		FlywayTestExecutionListener.class })
 @FlywayTest
 public class GithubServiceTest {
 
 	@Autowired
 	private GithubService githubService;
-	
+
+	private static final String PAYLOAD_FILE = "/json/githubPayload.json";
+
 	@Test
 	public void testUpdateGithubData() {
-		//fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
-	public void testGetPayloadHistory() {
-		//githubService.getPayloadHistory();
+	public void testGetPayloadHistory() throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		InputStream is = Test.class.getResourceAsStream(PAYLOAD_FILE);
+		Payload payload = mapper.readValue(is, Payload.class);
+		payload.setReceivedTimestamp(Calendar.getInstance().getTime());
+		githubService.updateGithubData(payload);
+		
+		githubService.getPayloadHistory();
 	}
 
 }
