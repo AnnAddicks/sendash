2745c34af035b821a67c08db6f60912633471f61
AnnAddicks <Ann.Addicks@gmail.com>
2015-08-28 14:56:13 -0400

    switched case.

--- src/test/resources/db/migration/V4.1__EndpointData.sql
+++ src/test/resources/db/migration/V4.1__EndpointData.sql
@@ -1,3 +1,3 @@
 
-INSERT INTO EndPoint (id, clientId, hostName, apiKey) VALUES ('zzfe6e4c-f45a-43ef-8ca7-d2219b509099', '54fe6e4c-f45a-43ef-8ca7-d2219b5090b4', 'this is a host name.', 'H9cZ0GA5WgaP');
-INSERT INTO EndPoint (id, clientId, hostName, apiKey) VALUES ('zzfe6e4c-f45a-43ef-8ca7-d2219b509988', '54fe6e4c-f45a-43ef-8ca7-d2219b5090b4', 'another host name.', 'H9c!!!A5WgaP');
+INSERT INTO EndPoint (ID, CLIENT_ID, HOST_NAME, API_KEY) VALUES ('zzfe6e4c-f45a-43ef-8ca7-d2219b509099', '54fe6e4c-f45a-43ef-8ca7-d2219b5090b4', 'this is a host name.', 'H9cZ0GA5WgaP');
+INSERT INTO EndPoint (ID, CLIENT_ID, HOST_NAME, API_KEY) VALUES ('zzfe6e4c-f45a-43ef-8ca7-d2219b509988', '54fe6e4c-f45a-43ef-8ca7-d2219b5090b4', 'another host name.', 'H9c!!!A5WgaP');
