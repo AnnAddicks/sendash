18629c9b657f70c6a690240eaa35797993bec49c
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-01 10:27:11 -0400

    spring boilerplate for controller and test.

--- src/main/java/com/addicks/sendash/admin/api/rest/FileController.java
+++ src/main/java/com/addicks/sendash/admin/api/rest/FileController.java
@@ -1,5 +1,13 @@
 package com.addicks.sendash.admin.api.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.Api;
+
+@RestController
+@RequestMapping(value = "file-manager")
+@Api(value = "status", description = "Status update for health check scripts")
 public class FileController {
 
 }
