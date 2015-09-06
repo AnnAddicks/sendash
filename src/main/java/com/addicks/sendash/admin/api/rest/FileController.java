f09c79b816e7b84940e9157abb0441e9a2a52a38
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-06 10:39:01 -0400

    working on fileController and test.

--- src/main/java/com/addicks/sendash/admin/api/rest/FileController.java
+++ src/main/java/com/addicks/sendash/admin/api/rest/FileController.java
@@ -6,6 +6,8 @@ import java.util.zip.ZipInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,9 +22,12 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping(value = "file-manager")
+@RequestMapping(value = FileController.REQUEST_MAPPING)
 @Api(value = "status", description = "Status update for health check scripts")
 public class FileController {
+  public static final String REQUEST_MAPPING = "file-manager";
+
+  private static final Logger log = LoggerFactory.getLogger(FileController.class);
 
   @Autowired
   private FileService fileService;
@@ -31,14 +36,14 @@ public class FileController {
 
   }
 
-  @RequestMapping(value = "/zip", method = RequestMethod.POST, consumes = { "application/json",
-      "application/xml" }, produces = { "application/zip" })
+  @RequestMapping(value = "/zip", method = RequestMethod.GET, produces = { "application/zip" })
   @ResponseStatus(HttpStatus.OK)
   @ApiOperation(value = "Get a zip containing scripts of the powershell directory.", notes = "Returns all files in the directory in the zip.")
   public ResponseEntity<ZipInputStream> getZipedScripts(HttpServletRequest request,
       HttpServletResponse response) {
-
+    log.error("made it to controller");
     try (ZipInputStream fileInputStream = fileService.getZip()) {
+      log.error("We have the zip!");
       return ResponseEntity.ok().contentType(MediaType.parseMediaType("application/octet-stream"))
           .body(fileInputStream);
     }
@@ -46,6 +51,7 @@ public class FileController {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
+    log.error("not good!");
     return null;
 
   }
--- src/test/java/com/addicks/sendash/admin/api/rest/test/FileControllerTest.java
+++ src/test/java/com/addicks/sendash/admin/api/rest/test/FileControllerTest.java
@@ -1,16 +1,30 @@
 package com.addicks.sendash.admin.api.rest.test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.addicks.sendash.admin.Application;
-import com.addicks.sendash.admin.domain.properties.RepositoryProperties;
+import com.addicks.sendash.admin.api.rest.FileController;
 
 @Profile("test")
 @ActiveProfiles("test")
@@ -19,11 +33,24 @@ import com.addicks.sendash.admin.domain.properties.RepositoryProperties;
 @SpringApplicationConfiguration(classes = Application.class)
 public class FileControllerTest {
 
+  @InjectMocks
+  private FileController controller;
+
   @Autowired
-  private RepositoryProperties repositoryProperties;
+  private WebApplicationContext context;
+
+  private MockMvc mvc;
+
+  @Before
+  public void initTests() throws IOException {
+    MockitoAnnotations.initMocks(this);
+    mvc = MockMvcBuilders.webAppContextSetup(context).build();
+  }
 
   @Test
-  public void testNothing() {
-
+  public void shouldReturnZip() throws Exception {
+    mvc.perform(get("http://localhost/" + FileController.REQUEST_MAPPING + "/zip")
+        .accept(MediaType.APPLICATION_OCTET_STREAM)).andExpect(status().isOk())
+        .andExpect(content().contentType("application/zip")).andDo(MockMvcResultHandlers.print());
   }
 }
