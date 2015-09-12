a10ad6bf57400bcbddd38fab565979f747f762f5
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-11 21:15:51 -0400

    add table fields to the user object.

--- src/main/java/com/addicks/sendash/admin/domain/User.java
+++ src/main/java/com/addicks/sendash/admin/domain/User.java
@@ -25,12 +25,42 @@ public class User implements UserDetails {
   @GeneratedValue()
   private long id;
 
+  private String email;
+
+  private String firstName;
+
+  private String lastName;
+
   public long getId() {
     return id;
   }
 
   public void setId(long id) {
     this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
   }
 
   @Override
@@ -79,7 +109,7 @@ public class User implements UserDetails {
   public int hashCode() {
     final int prime = 31;
     int result = 1;
-    result = prime * result + (int) (id ^ (id >>> 32));
+    result = prime * result + ((email == null) ? 0 : email.hashCode());
     return result;
   }
 
@@ -92,7 +122,11 @@ public class User implements UserDetails {
     if (getClass() != obj.getClass())
       return false;
     User other = (User) obj;
-    if (id != other.id)
+    if (email == null) {
+      if (other.email != null)
+        return false;
+    }
+    else if (!email.equals(other.email))
       return false;
     return true;
   }
