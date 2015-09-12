8e14d0e2f263278ddc36c50be9fc269e8e8ff03a
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-11 20:48:31 -0400

    user service & test.

---
+++ src/main/java/com/addicks/sendash/admin/service/UserService.java
@@ -0,0 +1,8 @@
+package com.addicks.sendash.admin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
---
+++ src/test/java/com/addicks/sendash/admin/service/test/UserServiceTest.java
@@ -0,0 +1,30 @@
+package com.addicks.sendash.admin.service.test;
+
+import org.flywaydb.test.annotation.FlywayTest;
+import org.flywaydb.test.junit.FlywayTestExecutionListener;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.addicks.sendash.admin.Application;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+
+@Profile("test")
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = Application.class)
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+    TransactionalTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
+    DbUnitTestExecutionListener.class, FlywayTestExecutionListener.class })
+@FlywayTest
+public class UserServiceTest {
+
+}
