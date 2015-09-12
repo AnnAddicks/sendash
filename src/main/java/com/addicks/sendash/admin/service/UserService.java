20810f9a758ab34f740ff1d300c6cc365dd04320
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-12 09:35:07 -0400

    implement the interface and auto-generate the method for now.

--- src/main/java/com/addicks/sendash/admin/service/UserService.java
+++ src/main/java/com/addicks/sendash/admin/service/UserService.java
@@ -2,7 +2,15 @@ package com.addicks.sendash.admin.service;
 
 import org.springframework.stereotype.Service;
 
+import com.addicks.sendash.admin.domain.User;
+
 @Service
-public class UserService {
+public class UserService implements IUserService {
+
+  @Override
+  public User save(User user) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
