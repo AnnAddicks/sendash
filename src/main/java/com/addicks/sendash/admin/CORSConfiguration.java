d8f332b5195b22e0fe74d9ff1efa9ee60b38590c
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-17 15:08:19 -0400

    using the new snapshot to add more CORS Configuration.

---
+++ src/main/java/com/addicks/sendash/admin/CORSConfiguration.java
@@ -0,0 +1,16 @@
+package com.addicks.sendash.admin;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CORSConfiguration {
+  // @Bean
+  // public WebMvcConfigurer corsConfigurer() {
+  // return new WebMvcConfigurerAdapter() {
+  // @Override
+  // public void addCorsMappings(CorsRegistry registry) {
+  // registry.addMapping("/api/**");
+  // }
+  // };
+  // }
+}
