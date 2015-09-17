7664ca515a389d16709a5d44cd6414f72c6bbdf2
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-17 11:10:44 -0400

    added put to CORS.

--- src/main/java/com/addicks/sendash/admin/api/rest/SimpleCORSFileter.java
+++ src/main/java/com/addicks/sendash/admin/api/rest/SimpleCORSFileter.java
@@ -20,7 +20,7 @@ public class SimpleCORSFileter implements Filter {
       throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
     response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
     chain.doFilter(req, res);
