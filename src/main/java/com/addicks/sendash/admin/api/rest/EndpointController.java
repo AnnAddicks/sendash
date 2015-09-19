60330c9bf016a126b47408cb426e52c840e9be71
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-19 10:31:03 -0400

    added CRUD to endpoint controller.

--- src/main/java/com/addicks/sendash/admin/api/rest/EndpointController.java
+++ src/main/java/com/addicks/sendash/admin/api/rest/EndpointController.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +18,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.addicks.sendash.admin.domain.Endpoint;
+import com.addicks.sendash.admin.exception.DataFormatException;
 import com.addicks.sendash.admin.service.IEndpointService;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = EndpointController.REQUEST_MAPPING)
@@ -28,6 +33,17 @@ public class EndpointController extends AbstractRestHandler {
 
   @Autowired
   private IEndpointService endpointService;
+
+  @RequestMapping(value = "", method = RequestMethod.POST, consumes = { "application/json",
+      "application/xml" }, produces = { "application/json", "application/xml" })
+  @ResponseStatus(HttpStatus.CREATED)
+  @ApiOperation(value = "Create a endpoint resource.", notes = "Returns the URL of the new resource in the Location header.")
+  public void createHotel(@RequestBody Endpoint endpoint, HttpServletRequest request,
+      HttpServletResponse response) {
+    Endpoint createdEndpoint = endpointService.create(endpoint);
+    response.setHeader("Location",
+        request.getRequestURL().append("/").append(createdEndpoint.getId()).toString());
+  }
 
   @RequestMapping(value = "", method = RequestMethod.GET, produces = { "application/json",
       "application/xml" })
@@ -44,4 +60,47 @@ public class EndpointController extends AbstractRestHandler {
     response.addHeader("X-Total-Count", "" + endpointPage.getNumberOfElements());
     return endpointPage.getContent();
   }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = { "application/json",
+      "application/xml" })
+  @ResponseStatus(HttpStatus.OK)
+  @ApiOperation(value = "Get a single endpoint.", notes = "You have to provide a valid endpoint ID.")
+  public @ResponseBody Endpoint getHotel(
+      @ApiParam(value = "The ID of the endpoint.", required = true) @PathVariable("id") Long id,
+      HttpServletRequest request, HttpServletResponse response) throws Exception {
+    Endpoint endpoint = endpointService.findById(id);
+    checkResourceFound(endpoint);
+    return endpoint;
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = { "application/json",
+      "application/xml" }, produces = { "application/json", "application/xml" })
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @ApiOperation(value = "Update an endpoint.", notes = "Provide a valid endpoint ID in the URL and in the payload. The ID attribute can not be updated.")
+  public void updateHotel(
+      @ApiParam(value = "The ID of the existing endpoint resource.", required = true) @PathVariable("id") Long id,
+      @RequestBody Endpoint endpoint, HttpServletRequest request, HttpServletResponse response) {
+    checkResourceFound(endpointService.findById(id));
+    if (id != endpoint.getId())
+      throw new DataFormatException("ID doesn't match!");
+    endpointService.update(endpoint);
+  }
+
+  @ApiResponses(value = {
+      // @ApiResponse(code = 400, message = "Invalid ID supplied",
+      // responseHeader = @ResponseHeader(name = "X-Rack-Cache", description =
+      // "Explains whether or not a cache was used", response = Boolean.class)
+      // ),
+      @ApiResponse(code = 404, message = "Endpoint not found") })
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = { "application/json",
+      "application/xml" })
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @ApiOperation(value = "Delete an endpoint.", notes = "You have to provide a valid endpoint ID in the URL. Once deleted the resource can not be recovered.")
+  public void deleteEndpoint(
+      @ApiParam(value = "The ID of the existing endpoint resource.", required = true) @PathVariable("id") Long id,
+      HttpServletRequest request, HttpServletResponse response) {
+    checkResourceFound(endpointService.findById(id));
+    endpointService.delete(id);
+  }
+
 }
