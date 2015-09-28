321c12aab04c89c046d44f3aea09c44648d1e660
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-28 15:01:01 -0400

    added generation strategy.

--- src/main/java/com/addicks/sendash/admin/domain/PendingEndpoint.java
+++ src/main/java/com/addicks/sendash/admin/domain/PendingEndpoint.java
@@ -3,6 +3,8 @@ package com.addicks.sendash.admin.domain;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -13,6 +15,7 @@ public class PendingEndpoint implements Serializable {
   private static final long serialVersionUID = 4863519288808297293L;
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @ManyToOne
