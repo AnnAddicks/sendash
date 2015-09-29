160375cbacc75d75de8e0839b3c719c693ac86d7
AnnAddicks <Ann.Addicks@gmail.com>
2015-09-29 15:21:02 -0400

    adding allow OPTIONS for CORS handling.

--- src/main/java/com/addicks/sendash/admin/api/rest/SimpleCORSFileter.java
+++ src/main/java/com/addicks/sendash/admin/api/rest/SimpleCORSFileter.java
@@ -24,6 +24,7 @@ public class SimpleCORSFileter implements Filter {
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
+    response.setHeader("Allow", "OPTIONS");
     chain.doFilter(req, res);
   }
 
