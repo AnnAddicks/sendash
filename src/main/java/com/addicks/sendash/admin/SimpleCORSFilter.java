978473241b21295f50f8bb8d1587f3f5baea8256
AnnAddicks <Ann.Addicks@gmail.com>
2015-10-08 10:08:41 -0400

    added the x-total-count to the headers to paginate tables on the GUI.

--- src/main/java/com/addicks/sendash/admin/SimpleCORSFilter.java
+++ src/main/java/com/addicks/sendash/admin/SimpleCORSFilter.java
@@ -23,7 +23,7 @@ public class SimpleCORSFilter implements Filter {
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
+        "Origin, X-Requested-With, Content-Type, Accept, x-total-count");
     response.setHeader("Allow", "OPTIONS");
     chain.doFilter(req, res);
   }
