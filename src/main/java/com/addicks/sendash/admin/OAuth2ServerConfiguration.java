671e8ad865d505cd559286d70a50e3a5c2041cfa
AnnAddicks <Ann.Addicks@gmail.com>
2015-10-14 15:21:33 -0400

    added new oauth tables and use the jdbc command for config instead of in memory.

--- src/main/java/com/addicks/sendash/admin/OAuth2ServerConfiguration.java
+++ src/main/java/com/addicks/sendash/admin/OAuth2ServerConfiguration.java
@@ -1,22 +1,25 @@
 package com.addicks.sendash.admin;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import com.addicks.sendash.admin.service.CustomUserDetailsService;
 
@@ -47,7 +50,10 @@ public class OAuth2ServerConfiguration {
   protected static class AuthorizationServerConfiguration
       extends AuthorizationServerConfigurerAdapter {
 
-    private TokenStore tokenStore = new InMemoryTokenStore();
+    @Autowired
+    private DataSource dataSource;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
     @Qualifier("authenticationManagerBean")
@@ -56,26 +62,36 @@ public class OAuth2ServerConfiguration {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
+    @Bean
+    public JdbcTokenStore tokenStore() {
+      return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+      return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+      security.passwordEncoder(passwordEncoder);
+    }
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-      endpoints.tokenStore(this.tokenStore).authenticationManager(this.authenticationManager)
-          .userDetailsService(userDetailsService);
+      endpoints.authorizationCodeServices(authorizationCodeServices())
+          .authenticationManager(authenticationManager).tokenStore(tokenStore())
+          .userDetailsService(userDetailsService).approvalStoreDisabled();
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-      clients.inMemory().withClient("sendashWebApp")
-          .authorizedGrantTypes("password", "refresh_token").authorities("USER")
-          .scopes("read", "write").resourceIds(RESOURCE_ID).secret("GoGoVCHeckProApp");
-    }
 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-      DefaultTokenServices tokenServices = new DefaultTokenServices();
-      tokenServices.setSupportRefreshToken(true);
-      tokenServices.setTokenStore(this.tokenStore);
-      return tokenServices;
+      clients.jdbc(dataSource).passwordEncoder(passwordEncoder).withClient("sendashWebApp")
+          .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+          .authorities("USER").scopes("read", "write").resourceIds(RESOURCE_ID)
+          .accessTokenValiditySeconds(60).secret("GoGoVCHeckProApp");
+
     }
 
   }
