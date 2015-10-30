80097af57e3a1f20b81eb85a7d43570896545b51
AnnAddicks <Ann.Addicks@gmail.com>
2015-10-30 10:21:43 -0400

    #7 new controller and test class to handle api calls to get referencial data between clients and users.

---
+++ src/test/java/com/addicks/sendash/admin/api/rest/test/UserClientControllerTest.java
@@ -0,0 +1,14 @@
+package com.addicks.sendash.admin.api.rest.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserClientControllerTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
