c984bdd2fa8b88ea587f6ac87a4c464168d4f851
Ann Addicks <ann.addicks@gmail.com>
2015-11-19 11:16:40 -0500

    #8 build script to build locally and on jenkins the same way.

--- build.sh
+++ build.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 export USER_CREATE_QUEUE="userCreateQueue"
+
+#=========================================
+#Start external apps
+#start rabbitmq
+#start smtp server
+
+#=========================================
+#Build & Run each Microservice
+
+#Email Queue
+cd registerUserQ/
+mvn clean install
+java -jar ./target/sendEmailQueue-0.1.0.jar&
+cd ..
+
+#Sendash API
+cd sendash
+mvn clean install
+cp ./target/sendash-0.1.0.war $CATALINA_HOME/webapps/sendash.war
+cd ..
+
+#=========================================
+#Run external integration tests
