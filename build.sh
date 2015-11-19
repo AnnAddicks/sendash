462a529aebd89b2dcc3675d3ca858e694e0f298e
Ann Addicks <ann.addicks@gmail.com>
2015-11-19 11:32:19 -0500

    #8 added variables to mvn's build to access git repo and the environment.sh to the build script.  Need to document in the readme.

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+source ./environmentSetup.sh
 export USER_CREATE_QUEUE="userCreateQueue"
 
 #=========================================
 #Start external apps
 #start rabbitmq
+rabbitmq-server -detached
 #start smtp server
 
 #=========================================
@@ -13,12 +15,12 @@ export USER_CREATE_QUEUE="userCreateQueue"
 #Email Queue
 cd registerUserQ/
 mvn clean install
-java -jar ./target/sendEmailQueue-0.1.0.jar&
+java -jar ./target/sendEmailQueue-0.1.0.jar > /var/log/sendash_email &
 cd ..
 
 #Sendash API
 cd sendash
-mvn clean install
+mvn clean install $MVN_STRING
 cp ./target/sendash-0.1.0.war $CATALINA_HOME/webapps/sendash.war
 cd ..
 
