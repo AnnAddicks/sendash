6e216f84e3e4533930d45baf96f61fa2737e782a
Ann Addicks <ann.addicks@gmail.com>
2015-11-20 11:37:00 -0500

    #8 stop the build script if a mvn project fails.

--- build.sh
+++ build.sh
@@ -14,19 +14,19 @@ rabbitmq-server -detached
 
 #Email Queue
 cd registerUserQ/
-mvn clean install
+mvn clean install || { echo "Maven build unsuccessful for Email Queue"; exit 1; }
 java -jar ./target/sendEmailQueue-0.1.0.jar > /var/log/sendash/sendash_email.log &
 cd ..
 
 #Sendash API
 cd sendash
-mvn clean install $MVN_STRING
+mvn clean install $MVN_STRING || { echo "Maven build unsuccessful for Sendash API"; exit 1; }
 cp ./target/sendash-0.1.0.war $CATALINA_HOME/webapps/sendash.war
 cd ..
 
 #=========================================
 #Run external integration tests
 cd sendash
-mvn test -Dspring.profiles.active=MicroserviceIntegrationTests
+mvn test -Dspring.profiles.active=MicroserviceIntegrationTests || { echo "Maven build unsuccessful for External Integration Tests"; exit 1; }
 cd ..
 
