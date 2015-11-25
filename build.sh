6e2c34c877bfb9170d045b2baa25bf4cda5bac74
Ann Addicks <ann.addicks@gmail.com>
2015-11-25 13:09:26 -0500

    set the active profile in the build script.

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ cd ..
 
 #Sendash API
 cd sendash
-mvn clean install $MVN_STRING || { echo "Maven build unsuccessful for Sendash API"; exit 1; }
+mvn clean install $MVN_STRING -Dspring.profiles.active=test || { echo "Maven build unsuccessful for Sendash API"; exit 1; }
 cp ./target/sendash-0.1.0.war $CATALINA_HOME/webapps/sendash.war
 cd ..
 
