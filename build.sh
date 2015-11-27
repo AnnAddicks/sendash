1d231fcf9f2238414cff192788015e11a980a3a7
Ann Addicks <ann.addicks@gmail.com>
2015-11-27 10:28:24 -0500

    added a parent pom and renamed the register project to an email project.

--- build.sh
+++ build.sh
@@ -12,27 +12,28 @@ rabbitmq-server -detached
 #=========================================
 #Build & Run each Microservice
 
+#Build via parent
+mvn clean install $MVN_STRING -Dspring.profiles.active=test || { echo "Maven build unsuccessful for Sendash project"; exit 1; }
+
 #Email Queue
 cd registerUserQ/
-mvn clean install || { echo "Maven build unsuccessful for Email Queue"; exit 1; }
 java -jar ./target/sendEmailQueue-0.1.0.jar > /var/log/sendash/sendash_email.log &
 cd ..
 
 #Sendash API
 cd sendash
-mvn clean install $MVN_STRING -Dspring.profiles.active=test || { echo "Maven build unsuccessful for Sendash API"; exit 1; }
 
 #Deploy to tomcat
+rm -rf /var/lib/tomcat7/webapps/sendash*
+rm -rf /var/lib/tomcat7/work/Catalina/localhost/sendash
 cp ./target/sendash-0.1.0.war $CATALINA_HOME/webapps/sendash.war
+service tomcat7 restart
 cd ..
 
 #=========================================
 #Run external integration tests
 cd sendash
 
-echo "************************"
-echo "USER QUEUE $USER_CREATE_QUEUE"
-echo "************************"
 mvn clean test  -Dtest=com.addicks.sendash.admin.test.TestQ -Dspring.profiles.active=MicroserviceIntegrationTests || { echo "Maven build unsuccessful for External Integration Tests"; exit 1; }
 cd ..
 
