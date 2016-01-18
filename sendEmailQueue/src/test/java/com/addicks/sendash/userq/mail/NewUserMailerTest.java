d5925c0a7803e9f1f435627403d178ed57fd7899
Ann Addicks <ann.addicks@gmail.com>
2016-01-18 15:08:23 -0500

    added configuration bean as workaround for spring boot and mockito's mock.

--- sendEmailQueue/src/test/java/com/addicks/sendash/userq/mail/NewUserMailerTest.java
+++ sendEmailQueue/src/test/java/com/addicks/sendash/userq/mail/NewUserMailerTest.java
@@ -1,42 +1,55 @@
 package com.addicks.sendash.userq.mail;
 
+import static org.mockito.Matchers.any;
+
 import java.util.UUID;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.addicks.sendash.userq.Application;
 
-import freemarker.template.Configuration;
+import static org.mockito.Mockito.verify;
 
 @Profile("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class NewUserMailerTest {
 
-  @Mock
-  private JavaMailSender javaMailService;
+  @Autowired
+  private JavaMailSender javaMailSender;
 
   @Autowired
-  private Configuration configuration;
+  private freemarker.template.Configuration configuration;
 
   private NewUserMailer newUserMailer;
 
+  @Configuration
+  public static class Config {
+    @Bean
+    JavaMailSender javaMailSender() {
+      return Mockito.mock(JavaMailSender.class);
+    }
+  }
+
   @Test
   public void testWelcomeNewUser() {
-    newUserMailer = new NewUserMailer(javaMailService, configuration);
+    newUserMailer = new NewUserMailer(javaMailSender, configuration);
     String email = "test@test.com";
     String uuid = UUID.randomUUID().toString();
 
     newUserMailer.welcomeNewUser(email, uuid);
 
-    verify(javaMailService).send(any());
+    verify(javaMailSender).send(any(SimpleMailMessage.class));
   }
 
 }
